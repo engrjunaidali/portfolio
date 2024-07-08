06cc046706a44e91b22142d198153112111c3171
Apptycoons <apptycoons@MacBook-Air.local>
2024-07-08 15:43:01 +0500

    initial

---
+++ index.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Junaid Ali | Portfolio</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="description" content="Leven - Resume / CV / vCard Template" />
+    <meta name="keywords" content="vcard, resposnive, retina, resume, jquery, css3, bootstrap, portfolio" />
+    <meta name="author" content="lmpixels" />
+    <link rel="shortcut icon" href="favicon.ico" />
+
+    <link rel="stylesheet" href="css/normalize.css" type="text/css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/owl.carousel.css" type="text/css" />
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+    <link rel="stylesheet" href="css/main.css" type="text/css" />
+
+    <script src="js/modernizr.custom.js"></script>
+</head>
+
+<body class="page">
+    <div class="lm-animated-bg"></div>
+
+    <!-- Loading animation -->
+    <div class="preloader">
+        <div class="preloader-animation">
+            <div class="preloader-spinner"></div>
+        </div>
+    </div>
+    <!-- /Loading animation -->
+
+    <!-- Scroll To Top Button -->
+    <div class="lmpixels-scroll-to-top"><i class="lnr lnr-chevron-up"></i></div>
+    <!-- /Scroll To Top Button -->
+
+    <div class="page-scroll">
+        <div id="page_container" class="page-container bg-move-effect" data-animation="transition-flip-in-right">
+
+            <?php include __DIR__ . "/partials/header.php"; ?>
+
+            <div id="main" class="site-main">
+                <div id="main-content" class="single-page-content">
+                    <div id="primary" class="content-area">
+                        <div id="content" class="page-content site-content single-post" role="main">
+                            <!-- Home Page Top Part -->
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12">
+                                    <div class="home-content">
+                                        <div class="row flex-v-align">
+                                            <div class="col-sm-12 col-md-5 col-lg-5">
+                                                <div class="home-photo">
+                                                    <div class="hp-inner" style="
+ background-image: url(./img/main_photo.jpg);
+                              "></div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-sm-12 col-md-7 col-lg-7">
+                                                <div class="home-text hp-left">
+                                                    <div class="owl-carousel text-rotation">
+                                                        <div class="item">
+                                                            <h4>backend-developer</h4>
+                                                        </div>
+
+                                                        <div class="item">
+                                                            <h4>full-stack-developer</h4>
+                                                        </div>
+                                                    </div>
+
+                                                    <h1>Junaid Ali</h1>
+                                                    <p>
+                                                        I specialize in creating captivating websites and dynamic web applications using Laravel and React JS. My expertise extends to crafting seamless desktop database solutions with MS Access, ensuring efficiency and functionality.
+                                                    </p>
+
+                                                    <div class="home-buttons">
+                                                        <a href="#" target="_blank" class="btn btn-primary">Download CV</a>
+                                                        <a href="#" target="_self" class="btn btn-secondary">Contact</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Home Page Top Part -->
+
+                            <?php include __DIR__ . "/sections/portfolio.php"; ?>
+
+                            <?php include __DIR__ . "/sections/services.php"; ?>
+
+
+
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12">
+                                    <div class="p-20"></div>
+
+                                    <!-- Testimonials -->
+                                    <div class="block-title">
+                                        <h2>Testimonials</h2>
+                                    </div>
+
+                                    <div id="testimonials_1" class="testimonials owl-carousel" data-mobile-items="1" data-tablet-items="2" data-items="2">
+                                        <!-- Testimonial 1 -->
+                                        <div class="testimonial-item">
+                                            <!-- Testimonial Content -->
+                                            <div class="testimonial-content">
+                                                <!-- Picture -->
+                                                <div class="testimonial-picture">
+                                                    <img src="./img/testimonials/testimonial-1.jpg" alt="Billy Adams" />
+                                                </div>
+                                                <!-- /Picture -->
+
+                                                <!-- Testimonial Text -->
+                                                <div class="testimonial-text">
+                                                    <p>
+                                                        Donec eu est vel metus consequat volutpat. Nunc
+                                                        aliquet euismod mauris, a feugiat urna ullamcorper
+                                                        non.
+                                                    </p>
+                                                </div>
+                                                <!-- /Testimonial Text -->
+
+                                                <!-- Testimonial author information -->
+                                                <div class="testimonial-author-info">
+                                                    <h5 class="testimonial-author">Billy Adams</h5>
+                                                    <p class="testimonial-firm">Rolling Thunder</p>
+                                                </div>
+                                                <!-- /Testimonial author information -->
+
+                                                <div class="testimonial-icon">
+                                                    <i class="fa fa-quote-left"></i>
+                                                </div>
+
+                                                <div class="testimonial-icon-big">
+                                                    <i class="fa fa-quote-right"></i>
+                                                </div>
+                                            </div>
+                                            <!-- /Testimonial Content -->
+                                        </div>
+                                        <!-- End of Testimonial 1 -->
+
+                                        <!-- Testimonial 2 -->
+                                        <div class="testimonial-item">
+                                            <!-- Testimonial Content -->
+                                            <div class="testimonial-content">
+                                                <!-- Picture -->
+                                                <div class="testimonial-picture">
+                                                    <img src="./img/testimonials/testimonial-2.jpg" alt="Gary Johnson" />
+                                                </div>
+                                                <!-- /Picture -->
+
+                                                <!-- Testimonial Text -->
+                                                <div class="testimonial-text">
+                                                    <p>
+                                                        Nam tempor commodo mi id sodales. Aenean sit amet
+                                                        nibh nec sapien consequat porta a sit amet diam.
+                                                    </p>
+                                                </div>
+                                                <!-- /Testimonial Text -->
+
+                                                <!-- Testimonial author information -->
+                                                <div class="testimonial-author-info">
+                                                    <h5 class="testimonial-author">Gary Johnson</h5>
+                                                    <p class="testimonial-firm">Locost Accessories</p>
+                                                </div>
+                                                <!-- /Testimonial author information -->
+
+                                                <div class="testimonial-icon">
+                                                    <i class="fa fa-quote-left"></i>
+                                                </div>
+
+                                                <div class="testimonial-icon-big">
+                                                    <i class="fa fa-quote-right"></i>
+                                                </div>
+                                            </div>
+                                            <!-- /Testimonial Content -->
+                                        </div>
+                                        <!-- End of Testimonial 2 -->
+
+                                        <!-- End of Testimonial 3 -->
+                                        <div class="testimonial-item">
+                                            <!-- Testimonial Content -->
+                                            <div class="testimonial-content">
+                                                <!-- Picture -->
+                                                <div class="testimonial-picture">
+                                                    <img src="./img/testimonials/testimonial-3.jpg" alt="Daniel Pringle" />
+                                                </div>
+                                                <!-- /Picture -->
+
+                                                <!-- Testimonial Text -->
+                                                <div class="testimonial-text">
+                                                    <p>
+                                                        Etiam pretium ipsum quis justo dictum accumsan.
+                                                        Phasellus egestas odio a velit scelerisque.
+                                                    </p>
+                                                </div>
+                                                <!-- /Testimonial Text -->
+
+                                                <!-- Testimonial author information -->
+                                                <div class="testimonial-author-info">
+                                                    <h5 class="testimonial-author">Daniel Pringle</h5>
+                                                    <p class="testimonial-firm">Sagebrush</p>
+                                                </div>
+                                                <!-- /Testimonial author information -->
+
+                                                <div class="testimonial-icon">
+                                                    <i class="fa fa-quote-left"></i>
+                                                </div>
+
+                                                <div class="testimonial-icon-big">
+                                                    <i class="fa fa-quote-right"></i>
+                                                </div>
+                                            </div>
+                                            <!-- /Testimonial Content -->
+                                        </div>
+                                        <!-- End of Testimonial 3 -->
+                                    </div>
+                                    <!-- /Testimonials -->
+
+                                    <div class="p-40"></div>
+
+                                    <!-- Clients Slider -->
+                                    <div class="block-title">
+                                        <h2>Clients</h2>
+                                    </div>
+
+                                    <div id="clients_1" class="clients owl-carousel" data-mobile-items="1" data-tablet-items="3" data-items="6">
+                                        <div class="client-block">
+                                            <a href="#" target="_blank" title="Golden Grid">
+                                                <img src="./img/clients/client-1.png" alt="Golden Grid" />
+                                            </a>
+                                        </div>
+
+                                        <div class="client-block">
+                                            <a href="#" target="_blank" title="Sweety">
+                                                <img src="./img/clients/client-2.png" alt="Sweety" />
+                                            </a>
+                                        </div>
+
+                                        <div class="client-block">
+                                            <a href="#" target="_blank" title="Magic Furnitures">
+                                                <img src="./img/clients/client-3.png" alt="Magic Furnitures" />
+                                            </a>
+                                        </div>
+
+                                        <div class="client-block">
+                                            <a href="#" target="_blank" title="LifeGuard">
+                                                <img src="./img/clients/client-4.png" alt="LifeGuard" />
+                                            </a>
+                                        </div>
+
+                                        <div class="client-block">
+                                            <a href="#" target="_blank" title="LESS">
+                                                <img src="./img/clients/client-5.png" alt="LESS" />
+                                            </a>
+                                        </div>
+
+                                        <div class="client-block">
+                                            <a href="#" target="_blank" title="Design Lovers">
+                                                <img src="./img/clients/client-6.png" alt="Design Lovers" />
+                                            </a>
+                                        </div>
+
+                                        <div class="client-block">
+                                            <a href="#" target="_blank" title="WordPress">
+                                                <img src="./img/clients/client-7.png" alt="WordPress" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <!-- /Clients Slider -->
+
+                                    <div class="p-40"></div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <?php include __DIR__ . "/partials/footer.php"; ?>
+
+        </div>
+    </div>
+
+    <script src="js/jquery-2.1.3.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.shuffle.min.js"></script>
+    <script src="js/masonry.pkgd.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCrDf32aQTCVENBhFJbMBKOUTiUAABtC2o"></script>
+    <script src="js/jquery.googlemap.js"></script>
+    <script src="js/validator.js"></script>
+    <script src="js/main.js"></script>
+    <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"rayId":"89ff1ae4dd659080","r":1,"version":"2024.4.1","token":"94b99c0576dc45bf9d669fb5e9256829"}' crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ sections/services.php
@@ -0,0 +1,89 @@
+<!-- Services -->
+<div class="row">
+    <div class="col-xs-12 col-sm-12">
+        <div class="p-50"></div>
+
+        <div class="block-title">
+            <h2>What I Do</h2>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-xs-12 col-sm-6">
+        <div class="info-list-w-icon">
+            <div class="info-block-w-icon">
+                <div class="ci-icon">
+                    <i class="linecons linecons-pen"></i>
+                </div>
+
+                <div class="ci-text">
+                    <h4>Copywrite</h4>
+                    <p>
+                        Mauris neque libero, aliquet vel mollis nec, euismod
+                        sed tellus. Mauris convallis dictum elit id
+                        volutpat. Vivamus blandit, dolor vitae lacinia
+                        maximus, risus velit vehicula odio, a tincidunt
+                        turpis turpis tempus ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="info-block-w-icon">
+                <div class="ci-icon">
+                    <i class="linecons linecons-display"></i>
+                </div>
+
+                <div class="ci-text">
+                    <h4>Web Design</h4>
+                    <p>
+                        Mauris neque libero, aliquet vel mollis nec, euismod
+                        sed tellus. Mauris convallis dictum elit id
+                        volutpat. Vivamus blandit, dolor vitae lacinia
+                        maximus, risus velit vehicula odio, a tincidunt
+                        turpis turpis tempus ex.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-6">
+        <div class="info-list-w-icon">
+            <div class="info-block-w-icon">
+                <div class="ci-icon">
+                    <i class="linecons linecons-shop"></i>
+                </div>
+
+                <div class="ci-text">
+                    <h4>Ecommerce</h4>
+                    <p>
+                        Mauris neque libero, aliquet vel mollis nec, euismod
+                        sed tellus. Mauris convallis dictum elit id
+                        volutpat. Vivamus blandit, dolor vitae lacinia
+                        maximus, risus velit vehicula odio, a tincidunt
+                        turpis turpis tempus ex.
+                    </p>
+                </div>
+            </div>
+
+            <div class="info-block-w-icon">
+                <div class="ci-icon">
+                    <i class="linecons linecons-megaphone"></i>
+                </div>
+
+                <div class="ci-text">
+                    <h4>Marketing</h4>
+                    <p>
+                        Mauris neque libero, aliquet vel mollis nec, euismod
+                        sed tellus. Mauris convallis dictum elit id
+                        volutpat. Vivamus blandit, dolor vitae lacinia
+                        maximus, risus velit vehicula odio, a tincidunt
+                        turpis turpis tempus ex.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Services -->
